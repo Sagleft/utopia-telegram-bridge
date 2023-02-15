0f0ee1292ee20ff84f9d04c0f1aa2525dd0105a0
Sagleft <sagleft@inbox.ru>
2023-02-15 13:40:48 +0200

    upload base

---
+++ bot.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	swissknife "github.com/Sagleft/swiss-knife"
+	"github.com/Sagleft/uchatbot-engine"
+	utopiago "github.com/Sagleft/utopialib-go/v2"
+	"github.com/Sagleft/utopialib-go/v2/pkg/structs"
+	"github.com/fatih/color"
+)
+
+const (
+	configFilePath = "config.json"
+)
+
+type config struct {
+	Utopia utopiago.Config `json:"utopia"`
+	Chats  []uchatbot.Chat `json:"chats"`
+}
+
+func main() {
+	cfg := config{}
+	if err := swissknife.ParseStructFromJSONFile(configFilePath, &cfg); err != nil {
+		color.Red("read config: %s", err.Error())
+		return
+	}
+
+	_, err := uchatbot.NewChatBot(uchatbot.ChatBotData{
+		Config: cfg.Utopia,
+		Chats:  cfg.Chats,
+		Callbacks: uchatbot.ChatBotCallbacks{
+			OnContactMessage:        OnContactMessage,
+			OnChannelMessage:        OnChannelMessage,
+			OnPrivateChannelMessage: OnPrivateChannelMessage,
+
+			WelcomeMessage: OnWelcomeMessage,
+		},
+		UseErrorCallback: true,
+		ErrorCallback:    onError,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func OnContactMessage(m structs.InstantMessage) {
+	fmt.Printf("[CONTACT] %s: %s\n", m.Nick, m.Text)
+}
+
+func OnChannelMessage(m structs.WsChannelMessage) {
+	fmt.Printf("[CHANNEL] %s: %s\n", m.Nick, m.Text)
+}
+
+func OnPrivateChannelMessage(m structs.WsChannelMessage) {
+	fmt.Printf("[PRIVATE] %s: %s\n", m.Nick, m.Text)
+}
+
+func OnWelcomeMessage(userPubkey string) string {
+	return fmt.Sprintf("Hello! Your pubkey is %s", userPubkey)
+}
+
+func onError(err error) {
+	color.Red(err.Error())
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module bot
+
+go 1.18
+
+require (
+	github.com/Sagleft/swiss-knife v1.2.1
+	github.com/Sagleft/uchatbot-engine v1.5.0
+	github.com/Sagleft/utopialib-go/v2 v2.3.0
+	github.com/fatih/color v1.14.1
+)
+
+require (
+	github.com/beefsack/go-rate v0.0.0-20220214233405-116f4ca011a0 // indirect
+	github.com/ctengiz/evtwebsocket v0.0.0-20180717104640-fc3583982591 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/sagleft/simple-cron v1.5.0 // indirect
+	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	gopkg.in/grignaak/tribool.v1 v1.0.0-20150312065122-d6bb19d816df // indirect
+)
